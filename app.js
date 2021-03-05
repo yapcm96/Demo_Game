46be8a4993a7e95b5d3d232579dd0b168be0c3ff
Choon Meng Yap <yapcm96@hotmail.com>
2021-03-05 10:15:39 +0000

    added poison status and more options for actions

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 import {Game} from './lib/Game.js';
 
+
+// main app file to start the Game
+//new Game().initializeGame();
 new Game().initializeGame();
+// let time = await new Promise(t => setTimeout(t, 60));
+
+// export setTimeout(function(){
+//     let timeout = true;)
+// }, 5000)
+
+
+// do {
+//     time--;
+//     console.log(time);
+// }
+// while (time < 60) {
+//     if (time == 0) {
+//         console.log("Time ran out: GAME OVER!")
+
+//     }
+// }
